15a0b5866751de8487deb37a14dca5ceec4a62a0
skitoo <alexis@skitoo.net>
2015-05-08 11:30:12 +0200

    update models

--- djangocon2015/djangocon2015/todo/models.py
+++ djangocon2015/djangocon2015/todo/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
 class Task(models.Model):
     name = models.CharField(max_length=64)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL)
-    category = models.ManyToManyField(Category, related_name='tasks')
+    categories = models.ManyToManyField(Category, related_name='tasks')
     done = models.BooleanField(default=False)
 
     def __str__(self):
