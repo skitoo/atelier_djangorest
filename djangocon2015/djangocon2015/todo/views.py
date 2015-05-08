a7dc233d3c08ca33b682e927babd5b1fc7d763f3
skitoo <alexis@skitoo.net>
2015-05-08 12:02:56 +0200

    update views

--- djangocon2015/djangocon2015/todo/views.py
+++ djangocon2015/djangocon2015/todo/views.py
@@ -1,5 +1,7 @@
 
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import detail_route, list_route
 from . import serializers, models
 
 
@@ -11,3 +13,15 @@ class Category(viewsets.ModelViewSet):
 class Task(viewsets.ModelViewSet):
     queryset = models.Task.objects.all()
     serializer_class = serializers.Task
+
+    @list_route()
+    def mine(self, request):
+        queryset = self.filter_queryset(
+            self.get_queryset().filter(owner=request.user)
+        )
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
