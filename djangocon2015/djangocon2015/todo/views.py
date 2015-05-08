5fe24a0b1c8efe24e2f75aa41f4044988ceb49bf
skitoo <alexis@skitoo.net>
2015-05-08 11:12:21 +0200

    define views

--- djangocon2015/djangocon2015/todo/views.py
+++ djangocon2015/djangocon2015/todo/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from . import serializers, models
+
+
+class Category(viewsets.ModelViewSet):
+    query = models.Category.objects.all()
+    serializer_class = serializers.Category()
+
+
+class Task(viewsets.ModelViewSet):
+    query = models.Task.objects.all()
+    serializer_class = serializers.Task()
