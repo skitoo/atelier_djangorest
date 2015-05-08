d14f8268f1ba9cd2d2298306f661f9a76df1bd1b
skitoo <alexis@skitoo.net>
2015-05-08 10:54:52 +0200

    create models

--- djangocon2015/djangocon2015/todo/models.py
+++ djangocon2015/djangocon2015/todo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=64)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    category = models.ManyToManyField(Category, related_name='tasks')
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
